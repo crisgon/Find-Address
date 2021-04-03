77b7712e969dd7c05b42d06074d52766bb3c681b
Cristiano Gonçalves <cgoncalves@mimic.com.br>
2021-04-03 18:36:56 -0300

    feature: integrate with api.

---
+++ src/script.js
@@ -0,0 +1,51 @@
+const $cepInput = document.getElementById("cep-input");
+const $cepInfo = document.getElementById("cep-info");
+const $btnCopy = document.getElementById("btn-copy");
+const $addressForm = document.getElementById("address-form");
+const $addressNotFound = document.getElementById("not-found-message");
+
+let cepValue = "";
+
+let cepInfo = null;
+
+$cepInput.addEventListener("input", handleInputCepValue);
+
+$addressForm.addEventListener("submit", handleSubmitForm);
+
+function handleInputCepValue(event) {
+  cepValue = event.target.value;
+}
+
+async function handleSubmitForm(event) {
+  event.preventDefault();
+
+  await fetch(`https://viacep.com.br/ws/${cepValue}/json`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      cepInfo = data;
+    });
+
+  showInfoSection();
+  populateTextArea();
+}
+
+function showInfoSection() {
+  if (cepInfo !== null) {
+    $cepInfo.style.display = "block";
+    $btnCopy.style.display = "block";
+    $addressNotFound.style.display = "none";
+  }
+}
+
+function populateTextArea() {
+  $cepInfo.innerHTML = `
+  Bairro: ${cepInfo.bairro} &#13;&#10;
+  Cep: ${cepInfo.cep} &#13;&#10;
+  Complemento: ${cepInfo.complemento || "Não existe"} &#13;&#10;
+  Endereço: ${cepInfo.logradouro} &#13;&#10;
+  Cidade: ${cepInfo.localidade} &#13;&#10;
+  UF: ${cepInfo.uf}
+  `;
+}
